ae64dba4f2fc207db0d037666c7e3d7f71d4c53d
by-student-2017 <studentsctest@gmail.com>
2021-10-25 14:56:26 +0900

    ubuntu 18.04 LTS

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt -y update
+sudo apt -y install g++
+sudo apt -y install gfortran
+sudo apt -y install build-essential
+sudo apt -y install libopenmpi-dev
+sudo apt -y install unzip
+
+cp ./2NNMEAM-QEQ/modified_source_code/* ./src
+cp ./2NNMEAM-QEQ/modified_source_code/* ./src/QEQ
+
+cd ./lib/meam
+make -f Makefile.gfortran
+cd ./../../src
+
+make package-status
+make yes-meam
+make yes-mc
+make yes-USER-REAXC
+make mpi
+
---
+++ src/version.h
@@ -0,0 +1,1 @@
+#define LAMMPS_VERSION "14 May 2016"
